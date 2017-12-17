a3c5d4830ef6afc92ae7859b57b44f5f6a0a97eb
diffshare <diffshare@gmail.com>
2017-12-17 20:29:13 +0900

    ショートカットの変更

--- src/app/item-detail/item-detail.component.ts
+++ src/app/item-detail/item-detail.component.ts
@@ -49,17 +49,19 @@ export class ItemDetailComponent implements OnInit {
   @HostListener('window:keydown', ['$event'])
   async onKeyDown(event: KeyboardEvent) {
     console.log(event);
-    if (event.key === 'ArrowDown') {
+    if (event.key === 'n') {
+      event.preventDefault();
       await this.itemList.navigateNext(this.id);
     }
-    if (event.key === 'ArrowUp') {
+    if (event.key === 'p') {
+      event.preventDefault();
       await this.itemList.navigateBack(this.id);
     }
     if (event.key === 'e') {
       event.preventDefault();
       await this.router.navigate(['items', this.id, 'edit']);
     }
-    if (event.key === 'n') {
+    if (event.key === 't') {
       event.preventDefault();
       await this.router.navigate(['items', 'new']);
     }
